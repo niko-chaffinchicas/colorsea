244bcf2db53491f354ed332243ab10c36140c349
Niko Chaffin <niko@bixly.com>
2016-12-28 00:04:54 -0800

    Removed console.logs

--- server/api/colorschemes/controllers.js
+++ server/api/colorschemes/controllers.js
@@ -26,11 +26,10 @@ controllers.index = function(req, res) {
 // };
 
 controllers.show = function(req, res) {
-  console.log(req.params.id);
   if (req.params.id) {
     ColorScheme.findOne({ _id: req.params.id })
     .then(function(colorscheme) {
-      console.log(colorscheme.toJSON());
+      // console.log(colorscheme.toJSON());
       return res.send(colorscheme.toJSON());
     })
     .catch(function(err) {
@@ -40,26 +39,19 @@ controllers.show = function(req, res) {
 };
 
 controllers.update = function(req, res) {
-  console.log(req.params.id);
+  // console.log(req.params.id);
   if (req.params.id) {
-    // ColorScheme.findOne({ _id: req.params.id })
-    // .then(function(colorscheme) {
-    //   for (var prop in req.body) {
-    //     if (prop !== "_id" && prop !== "_creator") {
-    //       colorscheme[prop] = req.body[prop];
-    //     }
-    //   }
-    //   colorscheme.markModified('colors');
-    //   return colorscheme.save();
-    // })
-    console.log(req.body);
-    ColorScheme.findOneAndUpdate({ _id: req.params.id }, { $set: { colors: req.body.colors } }, { new: true })
+    // console.log(req.body);
+    ColorScheme.findOneAndUpdate(
+      { _id: req.params.id },
+      { $set: { colors: req.body.colors }, $inc: { __v: 1 } },
+      { new: true }
+    )
     .then(function(colorscheme) {
-      console.log("colorscheme changed:", colorscheme);
+      // console.log("colorscheme changed:", colorscheme);
       return res.send(colorscheme.toJSON());
     })
     .catch(function(err) {
-      console.log("CATCHING ERROR");
       console.log(err);
       return res.status(500).send(err.message);
     });
